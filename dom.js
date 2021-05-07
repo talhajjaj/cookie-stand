f48bd41df3c50da48a0bd93d27b41e9d69b720f5
talhajjaj <taima.hajaj@gmail.com>
2021-05-07 22:23:41 +0300

    solving lab09

--- dom.js
+++ dom.js
@@ -159,3 +159,20 @@ for(let ind=0; ind<hours.length;ind++){
 
   }
   
+  let storeform=document.getElementById('storeform');
+  storeform.addEventListener('submit', addStore);
+   function addStore(event) {
+    event.preventDefault();
+
+let storeName=event.target.storeName.value;
+let Min=event.target.MinNumber.value;
+let Max=event.target.MaxNumber.value;
+let Avg=event.target.AvgNumber.value;
+
+let newStore=new Store(storeName , Min,Max,Avg)
+newStore.randomCustNumber();
+newStore.salesCookies();
+newStore.render();
+// console.log(newStore);
+  }
+
---
+++ file.js
@@ -0,0 +1,10 @@
+// 'use strict'
+// let b =document.getElementById('b');
+// let divEl=document.getElementById('containe');
+
+// b.addEventListener('submit', cell )
+// function  cell () {
+//     let aEl=document.createElement('a');
+//     divEl.appendChild(aEl);
+//     aEl.textContent='this is from js';
+// }
